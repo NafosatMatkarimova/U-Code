782a3fe12d8094a8cba665d84242af84d3211468
Nafosat <Nafosat@gmail.com>
2023-04-17 14:41:11 +0500

    started

--- js/app.js
+++ js/app.js
@@ -4,3 +4,9 @@ const topbtn = document. querySelector("#topbtn")
 topbtn.addEventListener("click" , ()=> {
     
 })
+
+let first = null
+let nicname = null
+let names = "jjjjj"
+
+console.log(first ?? nicname ?? names);
